f014bbc68dae4fc025c3548bf2921e4a07d298a8
Irulan <jrebergen@gmail.com>
2019-05-08 20:31:13 +0200

    added one item

--- itemDB.lua
+++ itemDB.lua
@@ -2,4 +2,5 @@ IruTrack_itemDB = {
     {itemID=19019, zone="Molten Core", slot="Weapon", source="drop", tracked=true,},
     {itemID=18737, zone="Stratholme", slot="Weapon", source="drop", tracked=true,},
     {itemID=15806, zone="Scholomance", slot="Weapon", source="quest", tracked=true,},
+    {itemID=18696, zone="Scholomance", slot="Shield", source="drop", tracked=true,},
 }
